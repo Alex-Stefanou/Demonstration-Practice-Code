b97923133fd3914bc15b405b73bb39abf862f23d
Alex Stefanou <Alex-Stefanou@users.noreply.github.com>
2020-01-14 17:24:21 +0000

    Logical framework constructed
    also added case handling of incorrect number of operators

--- Calculator/calculator_script.js
+++ Calculator/calculator_script.js
@@ -1,14 +1,8 @@
-var result="0";
-var error =
-    ["Error: Non-numerical values entered",
-     "Error: Calculator can only accept one operator",
-     "Error: Division by 0 is undefined"];
-
 /* Function to handle button pressing:
 Enters respective characters into the input bar */
-function buttonPress(input, id){
+function buttonPress( input, id ) {
     console.log("Button press registered");
-    switch(id){
+    switch( id ) {
         case "ac":      return "";                  break;
         case "del":     return input.slice(0,-1);   break;
         case "plus":    return input + "+";         break;
@@ -32,8 +26,58 @@ function buttonPress(input, id){
     }
 }
 
-function compute(input){
+/* Function to resolve input string into computable chunks for compute() */
+function parseEqn (input) {
+    var equation = {operation: null, LHS: null, RHS: null, errorCode: 0};
+    console.log(`Input equation is ${input}`);
+    input = input.replace( /,/g , "." )  //Replace all decimal commas with decimal points
+    let opPlus =    (input.match(/\+/g)||[]).length;  //Counts number of operators (if null then 0)
+    let opMinus =   (input.match(/\-/g)||[]).length;
+    let opTimes =   (input.match(/\*/g)||[]).length;
+    let opDivide =  (input.match(/\//g)||[]).length;
 
+    let opTotal = opPlus + opMinus + opTimes + opDivide;
+    console.log(`Operators = ${opTotal}`);
+
+    if ( opTotal == 0 ) {       //No operator in input, return the input
+        equation.operation = input;
+        return equation;
+    }
+    else if ( opTotal > 1) {    //Too many operators, return error 1
+        equation.errorCode = 1
+        return equation;
+    }
+    return equation;
+}
+
+/* Function that describes error codes for output to user */
+function errorID (id) {
+    switch ( id ) {
+        case 1: return "Error: Too many operators";   break;
+        
+        default: return "An unknown error has occured";
+    }
+}
+
+/* Function to compute equation */
+function compute (input) {
+    //take input and verify
+    var equation = parseEqn(input)
+    if ( equation.errorCode != 0) return errorID(equation.errorCode);
+    else if ( equation.operation == input) return input;
+
+        //if illegal chars -> error Non-numerical values entered
+    //find and classify operator
+        //if no operator -> return input
+        //if >1 op -> error only one operator accepted
+    //split either side of operator into left and right
+        //if left is empty -> error no left hand term
+        //if right is empty -> error no right hand term
+        //if left or right has >1 decimal point -> error incorrect decimal point usage
+        //if right=0 && op=division -> error division by zero
+    //convert left and right to numbers (and commas to dots)
+    //calculate result
+    //return result
 }
 
 /* Core application: */
@@ -41,50 +85,16 @@ var vm = new Vue({
     el: '#calculator',
     data: {
         equation: "",
-        answer: result
+        answer: "0"
     },
     methods: {
         pushEqn: function(e) {
             this.equation = buttonPress(this.equation, e.target.id)
+        },
+        resolveEqn: function() {
+            console.log("Starting Computation");
+            this.answer = compute(this.equation);
+            console.log("Computation Complete")
         }
-    }/*,
-    computed: {
-        output: function() {
-            return compute(this.input)
-        }
-    }*/
-});
-
-
-/*
-var buttons = new Vue({
-    add_character: add_char(x) {
-        temp_result += x,
-        result = temp_result,
     }
-
-
-    methods:{
-        zero:   add_char("0"),
-        one:    add_char("1"),
-        two:    add_char("2"),
-        three:  add_char("3"),
-        four:   add_char("4"),
-        five:   add_char("5"),
-        six:    add_char("6"),
-        seven:  add_char("7"),
-        eight:  add_char("8"),
-        nine:   add_char("9"),
-        plus:   add_char("+"),
-        minus:  add_char("-"),
-        times:  add_char("*"),
-        divide: add_char("/"),
-        dot:    add_char("."),
-        pi:     "pi",
-        ans:    "ans",
-
-        del:    remove_char(),
-        clear:  remove_all(),
-    },
-})
-*/
+});

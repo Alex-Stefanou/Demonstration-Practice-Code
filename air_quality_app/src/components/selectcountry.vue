7bac773d9b56e21d02bcfd8c06d9462bd2429833
Alex Stefanou <Alex-Stefanou@users.noreply.github.com>
2020-01-29 10:35:09 +0000

    added pagination and page navigation

--- air_quality_app/src/components/selectcountry.vue
+++ air_quality_app/src/components/selectcountry.vue
@@ -14,8 +14,12 @@
         </tbody>
 
       </table>
-
     </div>
+
+    <span v-for="(page,j) in pageNumbers" :key="j">
+      <span @click="changePage" v-bind:id="page"> {{ page }} |</span>
+    </span>
+
   </div>
 </template>
 
@@ -25,50 +29,85 @@
 export default {
   name: 'selectCountry',
   
+  /* Tell store to initialise list of countries from api */
   mounted() {
     this.$store.dispatch("updateAllCountries");
   },
 
   data () {
     return {
-      search: "",
+      currentPage: 1,
       displayedCountries: [],
+      filteredCountries: [],
+      pageSize: 20,
+      search: "",
     }
   },
 
   computed: {
-    allCountries () {
+    allCountries () { //Clean data from api and store in an array that will not be altered
       let listCountries = []
       for (let i = 0; i < this.$store.getters.allCountries.length; i++) {
-        listCountries.push( this.$store.getters.allCountries[i].name );
+        listCountries.push( this.$store.getters.allCountries[i].name ); //load just the country names into a new array
       }
       
       for (let i = 0; i < listCountries.length; i++) {
-        while ( listCountries[i] == undefined ) {
+        while ( listCountries[i] == undefined ) { //Remove elements with no country name
           listCountries.splice ( i, 1);
         }
       }
-      return listCountries.sort();
+      return listCountries.sort();  //sort alphabetically
+    },
+
+    pageNumbers () { //calculate how many pages there should be + pushes to array for display
+      let pages = []
+      let maxPages = Math.ceil( this.filteredCountries.length / this.pageSize );
+      for ( let i = 0; i < maxPages; i++) {
+        pages.push( i + 1 );
+      }
+      return pages;
     }
   },
 
   watch: {
-    search: function() {
+    allCountries: function() { //When page first loads, makes sure the list displays
+      if (this.filteredCountries.length == 0 ) this.filteredCountries = this.allCountries;
+    },
+
+    currentPage: function() { //When user selects a page this calls a display change
+      this.displayPage( this.currentPage );
+    },
+
+    filteredCountries: function() { //When user uses search bring list back to page 1
+      this.displayPage( 1 )
+    },
+
+    search: function() {  //Filter list of countries on search
       this.filterCountries();
     }
   },
 
   methods: {
-    filterCountries () {
-      this.displayedCountries = [];
+    changePage (e) {  //Changes currentPage when user clicks a page number
+      this.currentPage = parseInt( e.toElement.id );
+    },
+
+    displayPage ( page ) { //Changes which section of the country list is displayed at a time i.e. different pages
+      --page;
+      this.displayedCountries = this.filteredCountries.slice( page * this.pageSize, (page + 1) * this.pageSize);
+    },
+
+    filterCountries () {  //Performs search anywhere in country name (case insensitive)
+      this.filteredCountries = [];
 
       for ( let i = 0; i < this.allCountries.length; i++) {
         if ( this.allCountries[i].toLowerCase().includes( this.search.toLowerCase() ) ) {
-          this.displayedCountries.push( this.allCountries[i] );
+          this.filteredCountries.push( this.allCountries[i] );
         }
       }
     }
   }
+  
 };
 </script>
 

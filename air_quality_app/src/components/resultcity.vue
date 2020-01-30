abce497657d449e4f8ecd36fc63e6060a0038666
Alex Stefanou <Alex-Stefanou@users.noreply.github.com>
2020-01-30 10:49:06 +0000

    testing obtaining measurements for results

--- air_quality_app/src/components/resultcity.vue
+++ air_quality_app/src/components/resultcity.vue
@@ -1,24 +1,55 @@
 <template>
   <div>
     <div>
-      <h1>Results from selecting a city will go here</h1>
+      <h1>Coordinates page will go here</h1>
+      <button @click="fetchAQ">Fetch AQ</button>
+      <button @click="next">Next City</button>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'resultCity',
 
   data() {
     return{
+      cities: [],
+      i: 0,
     };
   },
 
-  mounted() {
-  },
+  mounted() {},
 
   methods: {
+    fetchAQ: function() {
+      let that = this;
+      let oneWeekAgo = new Date();
+      oneWeekAgo.setDate( oneWeekAgo.getDate() - 7);
+
+      axios.get( "https://api.openaq.org/v1/measurements?limit=999", {
+          params: {
+            country: this.$store.state.selectedCountry[1],
+            city: this.$store.state.selectedCity,
+            date_from: oneWeekAgo,
+          }
+        })
+        .then (function (response) {
+          console.log(response);
+          that.cities = response.data.results;
+          console.log("data fetched");
+        })
+        .catch (function (error) {
+          console.log("An error has occured during fetchAQ()");
+          console.log(error);
+        })
+    },
+
+    next: function() {
+      this.i++
+    }
   },
 };
 </script>
